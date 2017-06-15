8d390ee813708aa22aa7a96e48221e961102c6ac
Santiago Schwarz <sschwarzpazos@gmail.com>
2017-06-15 14:24:32 +0200

    feat(ts): created api-marvel config

---
+++ src/config/api-marvel.config.ts
@@ -0,0 +1,18 @@
+let apiMarvel = {
+  uri: 'gateway.marvel.com'
+  , protocol: 'https://'
+  , port: ':443'
+  , version: '/v1'
+  , status: '/public'
+  , apikey: 'b6ec14effa925965bd776af6a56b42d9'
+  , ts: '1'
+  , hash: '3f4c62c116758d3539eac4cc30ce050d'
+  , characters: '/characters'
+  , comics: '/comics'
+  , creators: '/creators'
+  , events: '/events'
+  , series: '/series'
+  , stories: '/stories'
+};
+
+export const API_MARVEL = apiMarvel;
