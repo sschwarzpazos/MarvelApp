14a8deb2c3543d6da500a770f0b971f714b0b8dd
Santiago Schwarz <sschwarzpazos@gmail.com>
2017-06-15 12:58:32 +0200

    feat(index.pages): added EventsPage

--- src/pages/index.pages.ts
+++ src/pages/index.pages.ts
@@ -2,3 +2,4 @@ export { HomePage } from './home/home';
 export { CharactersPage } from './characters/characters';
 export { ComicsPage } from './comics/comics';
 export { CreatorsPage } from './creators/creators';
+export { EventsPage } from './events/events';
