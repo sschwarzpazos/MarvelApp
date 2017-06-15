b921ce420dd82e1a63f803c3a95365bb6815f0f7
Santiago Schwarz <sschwarzpazos@gmail.com>
2017-06-15 14:01:05 +0200

    feat(app.component): added sidemenu config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
+// import { HomePage } from '../pages/home/home';
+import { SIDEMENU, rootPage } from '../config/sidemenu.config';
 
 @Component({
   templateUrl: 'app.html'
@@ -11,17 +12,17 @@ import { HomePage } from '../pages/home/home';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = HomePage;
+  rootPage: any = rootPage;
 
-  pages: Array<{title: string, component: any}>;
+  pages: Array<{title: string, component: any}> = SIDEMENU;
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
-    this.pages = [
-      { title: 'Home', component: HomePage }
-    ];
+    // this.pages = [
+    //   { title: 'Home', component: HomePage }
+    // ];
 
   }
 
