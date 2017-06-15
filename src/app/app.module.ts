ea9c0810cd6060cbb9912712e5183d0ad89df9c3
Santiago Schwarz <sschwarzpazos@gmail.com>
2017-06-15 12:38:51 +0200

    feat(app.module): imported CharactersPage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,21 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+
+// pages
+import {
+  HomePage
+  , CharactersPage
+} from '../pages/index.pages';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+    MyApp
+    , HomePage
+    , CharactersPage
   ],
   imports: [
     BrowserModule,
@@ -19,8 +25,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+    MyApp
+    , HomePage
+    , CharactersPage
   ],
   providers: [
     StatusBar,
