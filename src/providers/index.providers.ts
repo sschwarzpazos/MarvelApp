5d88e56a1d1d935d5e116389e227130a67cd4eeb
Santiago Schwarz <sschwarzpazos@gmail.com>
2017-06-20 14:29:13 +0200

    feat(index.providers): exported EventProvider

--- src/providers/index.providers.ts
+++ src/providers/index.providers.ts
@@ -3,3 +3,4 @@ export { Provider } from './Provider';
 export { CharacterProvider } from './character/character';
 export { ComicProvider } from './comic/comic';
 export { CreatorProvider } from './creator/creator';
+export { EventProvider } from './event/event';
