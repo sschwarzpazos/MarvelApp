c164dbecf949daa3b177d718081eab7c01e430ab
Santiago Schwarz <sschwarzpazos@gmail.com>
2017-06-20 14:31:29 +0200

    feat(index.providers): exported SerieProvider

--- src/providers/index.providers.ts
+++ src/providers/index.providers.ts
@@ -4,3 +4,4 @@ export { CharacterProvider } from './character/character';
 export { ComicProvider } from './comic/comic';
 export { CreatorProvider } from './creator/creator';
 export { EventProvider } from './event/event';
+export { SerieProvider } from './serie/serie';
