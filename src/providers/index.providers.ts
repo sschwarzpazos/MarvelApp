accb7cf2dee89e850eb0acacf352df80d9ef9a8d
Santiago Schwarz <sschwarzpazos@gmail.com>
2017-06-20 14:27:11 +0200

    feat(index.providers): exported CreatorProvider

--- src/providers/index.providers.ts
+++ src/providers/index.providers.ts
@@ -2,3 +2,4 @@
 export { Provider } from './Provider';
 export { CharacterProvider } from './character/character';
 export { ComicProvider } from './comic/comic';
+export { CreatorProvider } from './creator/creator';
